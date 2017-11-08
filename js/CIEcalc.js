531d871858e05bbc7edfe5a389590541337acb77
AlionaV <33001567+AlionaV@users.noreply.github.com>
2017-11-09 00:10:25 +0300

    Update CIEcalc.js

--- js/CIEcalc.js
+++ js/CIEcalc.js
@@ -624,15 +624,15 @@ clearlayer(ctx3);
 
 if (rpy(parseFloat(mousePos.y)) < 0.430 && rpy(parseFloat(mousePos.y)) > 0.299 && rpx(parseFloat(mousePos.x)) < 0.585 && rpx(parseFloat(mousePos.x)) > 0.244) {
 
-if (rpx(parseFloat(mousePos.x)) < 0.49) {
+if (rpx(parseFloat(mousePos.x)) < 0.51) {
 	T = 10000;
 do  {T = T - 10;	
 }
-while (Math.abs(pllocusy(pllocusx(T),T) - rpy(parseFloat(mousePos.y))) > 0.01 && Math.abs(pllocusx(T) - rpx(parseFloat(mousePos.x))) > 0.01  && T > 1000)} else {
+while (Math.abs(pllocusy(pllocusx(T),T) - rpy(parseFloat(mousePos.y))) > 0.01 & Math.abs(pllocusx(T) - rpx(parseFloat(mousePos.x))) > 0.01  && T > 1000)} else {
 	T = 2255;
 	do  {T = T - 2;	
 }
-while (Math.abs(pllocusy(pllocusx(T),T) - rpy(parseFloat(mousePos.y))) > 0.01 && Math.abs(pllocusx(T) - rpx(parseFloat(mousePos.x))) > 0.01  && T > 1000)
+while (Math.abs(pllocusy(pllocusx(T),T) - rpy(parseFloat(mousePos.y))) > 0.01 & Math.abs(pllocusx(T) - rpx(parseFloat(mousePos.x))) > 0.01  && T > 1000)
 }
 	
 clearlayer(ctx3);
